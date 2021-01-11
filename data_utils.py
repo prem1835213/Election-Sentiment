196f9a60928a35b1250015b7fd497c7d811be5c3
Chris lzc <zclin@ucsd.edu>
2021-01-11 04:13:06 -0800

    created new function stemmed_text in data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -1,11 +1,18 @@
 import re
 import string
+import pandas as pd
 from nltk.corpus import stopwords
+from nltk.stem.porter import *
+from collections import defaultdict
 
 #################################
 
 
 def clean_text(text):
+    '''
+    Remove urls, mentions, punctuations, numbers, extra
+    whitespaces, stopwords and converts tweet to lower-case.
+    '''
     text = text.replace('\n', ' ')
     text = re.sub(r'http://\S+|https://\S+', '', text) # removes urls
     text = re.sub(r'(@\w+\b)', '', text) # removes mentions
@@ -21,3 +28,21 @@ def clean_text(text):
     text = [word for word in text.split() if not (word in stop_words)] # removes stopwords
     
     return text
+
+
+def stemmed_text(full_text):
+    '''
+    Stems the full_text input and returns a dataframe 
+    with the count of each distinct word. This function 
+    requires input to be cleaned first using clean_text.
+    '''
+    wordCount = defaultdict(int) # initialize wordcount dictionary
+    stemmer = PorterStemmer() # initialize word stemmer
+    for tweet in full_text:
+        for w in tweet:
+            w = stemmer.stem(w) # stem each individual word
+            wordCount[w] += 1 # increment wordcount
+    counts = [(wordCount[w], w) for w in wordCount]
+    counts.sort(reverse = True) # sort from most frequent to least frequent
+    output = pd.DataFrame(data = counts, columns = ['count', 'word'])
+    return output
